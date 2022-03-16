b55c6f4e9ee30b5e824856f03676235a76633b3e
Lipon Roy <liponbdbd@gmail.com>
2022-03-16 10:49:35 +0600

    search feild add

--- javascript/lipon.js
+++ javascript/lipon.js
@@ -1,14 +1,16 @@
 const myData = () => {
-	const API_KEY = '3796807274d6fb0086c149dac022a368';
-	const country = 'dhaka';
+	const Search_input = document.getElementById('search-input-me').value;
 
-	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_KEY}`)
+	const API_KEY = '3796807274d6fb0086c149dac022a368';
+	// const country = 'dhaka';
+
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Search_input}&appid=${API_KEY}`)
 		.then(response => response.json())
 		.then(data => ShowData(data));
 };
 
-myData();
-
 const ShowData = data => {
+	const kacu = (document.getElementById('dataOne').innerText = data.main.temp);
+
 	console.log(data);
 };
