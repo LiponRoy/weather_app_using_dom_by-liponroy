34114e7e8d7568b7d09c0f76dd2e8d853cce6956
Lipon Roy <liponbdbd@gmail.com>
2022-03-16 12:00:23 +0600

    display api data to web page

--- javascript/lipon.js
+++ javascript/lipon.js
@@ -6,11 +6,30 @@ const myData = () => {
 
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Search_input}&appid=${API_KEY}`)
 		.then(response => response.json())
-		.then(data => ShowData(data));
+		.then(data => {
+			if (data.main === undefined || data === undefined) {
+				// console.log('data not found');
+				document.getElementById('dataNotFound').style.display = 'block';
+				document.getElementById('allData').style.display = 'none';
+			} else {
+				document.getElementById('dataNotFound').style.display = 'none';
+				document.getElementById('allData').style.display = 'show';
+				ShowData(data);
+			}
+		});
 };
 
 const ShowData = data => {
-	const kacu = (document.getElementById('dataOne').innerText = data.main.temp);
+	// clear privious data
+	document.getElementById('dataCon').innerText = '';
+	document.getElementById('dataOne').innerText = '';
+	document.getElementById('dataTwo').innerText = '';
+	document.getElementById('dataThree').innerText = '';
+	// show data
+	document.getElementById('dataCon').innerText = data.sys.country;
+	document.getElementById('dataOne').innerText = data.name;
+	document.getElementById('dataTwo').innerText = data.main.temp;
+	document.getElementById('dataThree').innerText = data.weather[0].description;
 
 	console.log(data);
 };
